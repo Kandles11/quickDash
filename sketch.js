852a88c16e257b135d5af866503db7c08df0d54d
Mason Thomas <Kandleseatwicks@gmail.com>
2019-04-24 09:11:27 -0500

    added location services. made time bigger

--- sketch.js
+++ sketch.js
@@ -1,3 +1,12 @@
+let apiKey = 'key=5527428e3c00437db9b132303192003'
+let api = 'https://api.apixu.com/v1/current.json?'
+
+let userLocation = '0'
+
+function preload() {
+  roboto = loadFont('assets/Roboto-Bold.ttf');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -20,6 +29,24 @@ if (mn < 10) {
   mnFix = ('0' + mn)
 }
 
-
+  textFont(roboto);
+  textSize(60);
   text(hrFix + ":" + mnFix + ":" + scFix, 100,100);
+}
+
+
+function successCallback (position) {
+    console.log(position.coords.latitude); // 43.2132209
+    console.log(position.coords.longitude); // 27.9571503
+    userLocation = position.coords.latitude + ', ' + position.coords.longitude
+    console.log(userLocation);
+}
+
+function errorCallback (error) {
+    console.log(error.message);
+}
+
+if (navigator.geolocation) {
+  console.log('geo supported')
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 }
